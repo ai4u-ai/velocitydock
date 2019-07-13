2b836eca3253ccf1679913e03ec9fca23ef6551c
Ivan Jacobs <ivan.jacobs@touchapps.eu>
2019-07-13 08:13:17 +0200

    cors rules

--- vidStreamer.js
+++ vidStreamer.js
@@ -2597,10 +2597,8 @@ app.delete('/api/media/deleteAll', passport.authenticate('jwt', {session: false}
 app.use('/api', apiRoutes);
 
 // app.listen(port);
-//server.listen(port);
-server.listen(port, 'localhost', function() {
-    console.log("... port in %s mode", app.settings.env);
-});
+server.listen(port);
+
 app.listen(port, 'localhost', function() {
     console.log("... port %d mode",  app.settings.env);
 });
