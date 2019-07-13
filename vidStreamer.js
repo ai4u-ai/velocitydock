4edacf68ecd5ebad4119d5d5593d9bed4846391f
Ivan Jacobs <ivan.jacobs@touchapps.eu>
2019-07-13 07:46:46 +0200

    cors rules

--- vidStreamer.js
+++ vidStreamer.js
@@ -24,12 +24,25 @@ var express = require("express");
 var connectDomain = require('connect-domain');
 var app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(function (req, res, next) {
+    /*var err = new Error('Not Found');
+     err.status = 404;
+     next(err);*/
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+
+//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    // Pass to next layer of middleware
     next();
 });
-
 
 // app.use(connectDomain());
 var vidStreamer = require("vid-streamer");
